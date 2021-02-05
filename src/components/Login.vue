6e3d135269d2aad104b769fd871db3e865167e5f
WuNandi <10538481@qq.com>
2021-02-05 08:03:33 +0800

    files

---
+++ src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login_container">
+    登录界面
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="less" scoped=true>
+.login_container {
+  background-color: #2b4b6b;
+}
+</style>
