0ccd3e774dc141c48319f13bc9a5370b3a0a982f
WuNandi <10538481@qq.com>
2021-02-09 12:59:26 +0800

    login function complete

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>
-    <el-button type='info' @click="signOut">退出</el-button>
-  </div>
+  <!-- 用的是element container布局 -->
+  <!-- home页面 -->
+  <el-container class="home-container">
+    <!-- home 头部 -->
+  <el-header>
+    <div>
+      <img src="../assets/images/heima.png" alt="">
+      <span>品优购后台管理系统</span>
+    </div>
+    <el-button type='info' @click="signOut">退出</el-button></el-header>
+  <!-- home 内容区 -->
+  <el-container>
+    <!-- 内容左侧栏 -->
+    <el-aside width="200px">Aside</el-aside>
+    <!-- 主内容区 -->
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
 </template>
 <script>
 export default {
@@ -14,4 +29,31 @@ export default {
 }
 </script>
 <style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  // flex布局需要好好补习一下
+  display: flex;
+  justify-content: space-between;
+  // button 上下居中就变小了？
+  align-items: center;
+  padding-left: 0;
+  background-color: #373d41;
+  font-size: 20px;
+  color: #fff;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 20px;
+    }
+  }
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #EAEDF1;
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,9 +1,23 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main
+} from 'element-ui'
 
 // 注册全局组件
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
 Vue.prototype.$message = Message
