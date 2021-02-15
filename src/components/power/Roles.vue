8c8ad19857d1676095d19efb13f6777580b3ccae
WuNandi <10538481@qq.com>
2021-02-15 15:11:22 +0800

    无效语句删除

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -11,7 +11,7 @@
       <!-- 添加角色按钮 -->
       <el-row>
         <el-col>
-          <el-button type="primary" @click="">添加角色</el-button>
+          <el-button type="primary">添加角色</el-button>
         </el-col>
       </el-row>
       <!-- 角色列表 -->
